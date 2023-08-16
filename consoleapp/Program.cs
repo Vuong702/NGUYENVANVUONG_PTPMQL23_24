00b68c8d15c68024b44e33c422cba981aaa5a6fa
Vuong702 <vuong180900@gmail.com>
2023-08-16 10:56:08 +0700

    switch..case

--- consoleapp/Program.cs
+++ consoleapp/Program.cs
@@ -2,18 +2,22 @@
 {
 private static void Main(string[] args) 
   {
- float a =10;
- if(a >=8){
-  System.Console.WriteLine("hoc sinh hoc luc gioi");
- } else if(a >=6.5){
-  System.Console.WriteLine("hoc sinh hoc luc kha");
- } else if(a >=5){
-  System.Console.WriteLine("hoc sinh hoc luc trung binh");
- } else if(a >=3.5){
-  System.Console.WriteLine("hoc sinh hoc luc yeu");
- }  else {
-  System.Console.WriteLine("hoc sinh hoc luc kem");
- }
+  int day = 2 ;
+  switch(day)
+  {
+    case 1 : System.Console.WriteLine("chu nhat");break;
+     case 2 : System.Console.WriteLine("thu hai");break;
+     case 3 : System.Console.WriteLine("thu ba");break;
+     case 4 : System.Console.WriteLine("thu tu");break;
+     case 5 : System.Console.WriteLine("thu nam");break;
+     case 6 : System.Console.WriteLine("thu sau");break;
+     case 7 : System.Console.WriteLine("thu bay");break;
+     default :
+     System.Console.WriteLine("khong phai ngay trong tuan");
+     break;
+
+  }
+ 
    
   }
 }
