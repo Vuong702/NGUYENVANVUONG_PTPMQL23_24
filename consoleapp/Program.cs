16c40723f5705c534d19806e73772e36a320f0ce
Vuong702 <vuong180900@gmail.com>
2023-08-16 10:20:41 +0700

    chuyendoidulieu

--- consoleapp/Program.cs
+++ consoleapp/Program.cs
@@ -2,8 +2,10 @@
 {
 private static void Main(string[] args) 
   {
- string hoTen = "NGUYEN VAN A" ;
- const int namLamviec = 8; 
-Console.WriteLine("Sinh vien {0} - {1} nam kinh nghiem",hoTen,namLamviec);
+ string str = "123";
+ int ketQua;
+ bool kiemTra = false;
+ kiemTra = int.TryParse(str,out(ketQua));
+ System.Console.WriteLine("ket Qua = " + ketQua);
   }
 }
