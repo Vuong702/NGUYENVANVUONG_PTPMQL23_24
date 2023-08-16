96f633942fa11baf9d09eb938524662c2ce8603e
Vuong702 <vuong180900@gmail.com>
2023-08-16 09:49:02 +0700

    16/8

--- consoleapp/Program.cs
+++ consoleapp/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+private static void Main(string[] args) 
+  {
+ Console.Write("Nhap vao du lieu: ");
+int a = Console.Read();
+Console.WriteLine("Du lieu nahp vao la:" + a);
+  }
+}
