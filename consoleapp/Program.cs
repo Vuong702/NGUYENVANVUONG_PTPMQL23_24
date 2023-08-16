ea976d4cb2a20ec0a2b64e7c848f8e361a65ea39
Vuong702 <vuong180900@gmail.com>
2023-08-16 11:01:28 +0700

    cautruclap for

--- consoleapp/Program.cs
+++ consoleapp/Program.cs
@@ -2,22 +2,10 @@
 {
 private static void Main(string[] args) 
   {
-  int day = 2 ;
-  switch(day)
-  {
-    case 1 : System.Console.WriteLine("chu nhat");break;
-     case 2 : System.Console.WriteLine("thu hai");break;
-     case 3 : System.Console.WriteLine("thu ba");break;
-     case 4 : System.Console.WriteLine("thu tu");break;
-     case 5 : System.Console.WriteLine("thu nam");break;
-     case 6 : System.Console.WriteLine("thu sau");break;
-     case 7 : System.Console.WriteLine("thu bay");break;
-     default :
-     System.Console.WriteLine("khong phai ngay trong tuan");
-     break;
-
-  }
- 
+ for(int i = 0; i < 10; i++)
+ {
+  System.Console.WriteLine("vong lap thu {0}",i);
+ }
    
   }
 }
