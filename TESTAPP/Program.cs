47fa9045f6ba8dac58147e1eb89c40fbd7810a3f
Vuong702 <vuong180900@gmail.com>
2023-08-28 23:30:00 +0700

    test

--- TESTAPP/Program.cs
+++ TESTAPP/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿﻿using System.Collections.Specialized;
+using TESTAPP.Models;
+
+public class Program 
+{
+    private static void Main (string[] args)
+    {
+        Test std = new Test();
+        std.EnterData();
+        std.Display();
+    }
+    
+
+}
